fff19308eb3145276436e81bc01c5b165c7b9f57
Sujon Mir <sujonmhk786@gmail.com>
2024-01-20 05:21:43 +0600

    3rd commit

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 // I tried my best to make the code as simple as possible please dont mind the variable names.
 // Also this idea came in my mind after checking Traversy Media's latest video.
 
-
 // Clear the concole on every refresh
 console.clear();
 // set the body to full height
@@ -11,11 +10,12 @@ console.clear();
 // Range Slider Properties.
 // Fill : The trailing color that you see when you drag the slider.
 // background : Default Range Slider Background
-let myPass = ''
+let myPass = "";
 let qr;
+let customText = document.getElementById("customText");
 const sliderProps = {
-    fill: "#0B1EDF",
-    background: "rgba(255, 255, 255, 0.214)",
+  fill: "#0B1EDF",
+  background: "rgba(255, 255, 255, 0.214)",
 };
 
 // Selecting the Range Slider container which will effect the LENGTH property of the password.
@@ -25,51 +25,55 @@ const slider = document.querySelector(".range__slider");
 const sliderValue = document.querySelector(".length__title");
 
 // Using Event Listener to apply the fill and also change the value of the text.
-slider.querySelector("input").addEventListener("input", event => {
-    sliderValue.setAttribute("data-length", event.target.value);
-    applyFill(event.target);
+slider.querySelector("input").addEventListener("input", (event) => {
+  sliderValue.setAttribute("data-length", event.target.value);
+  applyFill(event.target);
 });
 // Selecting the range input and passing it in the applyFill func.
 applyFill(slider.querySelector("input"));
 // This function is responsible to create the trailing color and setting the fill.
 function applyFill(slider) {
-    const percentage = (100 * (slider.value - slider.min)) / (slider.max - slider.min);
-    const bg = `linear-gradient(90deg, ${sliderProps.fill} ${percentage}%, ${sliderProps.background} ${percentage +
-			0.1}%)`;
-    slider.style.background = bg;
-    sliderValue.setAttribute("data-length", slider.value);
+  const percentage =
+    (100 * (slider.value - slider.min)) / (slider.max - slider.min);
+  const bg = `linear-gradient(90deg, ${sliderProps.fill} ${percentage}%, ${
+    sliderProps.background
+  } ${percentage + 0.1}%)`;
+  slider.style.background = bg;
+  sliderValue.setAttribute("data-length", slider.value);
 }
 
 // Object of all the function names that we will use to create random letters of password
 const randomFunc = {
-    lower: getRandomLower,
-    upper: getRandomUpper,
-    number: getRandomNumber,
-    symbol: getRandomSymbol,
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
 };
 
 // Random more secure value
 function secureMathRandom() {
-    return window.crypto.getRandomValues(new Uint32Array(1))[0] / (Math.pow(2, 32) - 1);
+  return (
+    window.crypto.getRandomValues(new Uint32Array(1))[0] / (Math.pow(2, 32) - 1)
+  );
 }
 
 // Generator Functions
 // All the functions that are responsible to return a random value taht we will use to create password.
 function getRandomLower() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
 function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
 function getRandomNumber() {
-    return String.fromCharCode(Math.floor(secureMathRandom() * 10) + 48);
+  return String.fromCharCode(Math.floor(secureMathRandom() * 10) + 48);
 }
 
 function getRandomSymbol() {
-    const symbols = '~!@#$%^&*()_+{}":?><;.,';
-    return symbols[Math.floor(Math.random() * symbols.length)];
+  const symbols = '~!@#$%^&*()_+{}":?><;.,$€£¥₣₹₻₽₾₺₼₸₴₷฿원₫₮₯₱₳₵₲₪₰';
+  return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
 // Selecting all the DOM Elements that are necessary -->
@@ -102,147 +106,147 @@ let generatedPassword = false;
 // Update Css Props of the COPY button
 // Getting the bounds of the result viewbox container
 let resultContainerBound = {
-    left: resultContainer.getBoundingClientRect().left,
-    top: resultContainer.getBoundingClientRect().top,
+  left: resultContainer.getBoundingClientRect().left,
+  top: resultContainer.getBoundingClientRect().top,
 };
 // This will update the position of the copy button based on mouse Position
-resultContainer.addEventListener("mousemove", e => {
-    resultContainerBound = {
-        left: resultContainer.getBoundingClientRect().left,
-        top: resultContainer.getBoundingClientRect().top,
-    };
-    if (generatedPassword) {
-        copyBtn.style.opacity = '1';
-        copyBtn.style.pointerEvents = 'all';
-        copyBtn.style.setProperty("--x", `${e.x - resultContainerBound.left}px`);
-        copyBtn.style.setProperty("--y", `${e.y - resultContainerBound.top}px`);
-    } else {
-        copyBtn.style.opacity = '0';
-        copyBtn.style.pointerEvents = 'none';
-    }
+resultContainer.addEventListener("mousemove", (e) => {
+  resultContainerBound = {
+    left: resultContainer.getBoundingClientRect().left,
+    top: resultContainer.getBoundingClientRect().top,
+  };
+  if (generatedPassword) {
+    copyBtn.style.opacity = "1";
+    copyBtn.style.pointerEvents = "all";
+    copyBtn.style.setProperty("--x", `${e.x - resultContainerBound.left}px`);
+    copyBtn.style.setProperty("--y", `${e.y - resultContainerBound.top}px`);
+  } else {
+    copyBtn.style.opacity = "0";
+    copyBtn.style.pointerEvents = "none";
+  }
 });
-window.addEventListener("resize", e => {
-    resultContainerBound = {
-        left: resultContainer.getBoundingClientRect().left,
-        top: resultContainer.getBoundingClientRect().top,
-    };
+window.addEventListener("resize", (e) => {
+  resultContainerBound = {
+    left: resultContainer.getBoundingClientRect().left,
+    top: resultContainer.getBoundingClientRect().top,
+  };
 });
 
 // Copy Password in clipboard
 copyBtn.addEventListener("click", () => {
-    const textarea = document.createElement("textarea");
-    const password = resultEl.innerText;
-    if (!password || password == "CLICK GENERATE") {
-        return;
-    }
-    textarea.value = password;
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand("copy");
-    textarea.remove();
+  const textarea = document.createElement("textarea");
+  const password = resultEl.innerText;
+  if (!password || password == "CLICK GENERATE") {
+    return;
+  }
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
 
-    copyInfo.style.transform = "translateY(200%)";
-    copyInfo.style.opacity = "0";
-    copiedInfo.style.transform = "translateY(0%)";
-    copiedInfo.style.opacity = "0.75";
+  copyInfo.style.transform = "translateY(200%)";
+  copyInfo.style.opacity = "0";
+  copiedInfo.style.transform = "translateY(0%)";
+  copiedInfo.style.opacity = "0.75";
 });
 
 // When Generate is clicked Password id generated.
 generateBtn.addEventListener("click", () => {
-    const length = +lengthEl.value;
-    const hasLower = lowercaseEl.checked;
-    const hasUpper = uppercaseEl.checked;
-    const hasNumber = numberEl.checked;
-    const hasSymbol = symbolEl.checked;
-    generatedPassword = true;
-    myPass = generatePassword(length, hasLower, hasUpper, hasNumber, hasSymbol);
-    resultEl.innerText = myPass
-    copyInfo.style.transform = "translateY(0%)";
-    copyInfo.style.opacity = "0.75";
-    copiedInfo.style.transform = "translateY(200%)";
-    copiedInfo.style.opacity = "0";
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numberEl.checked;
+  const hasSymbol = symbolEl.checked;
+  generatedPassword = true;
+  myPass = generatePassword(length, hasLower, hasUpper, hasNumber, hasSymbol);
+  copyInfo.style.transform = "translateY(0%)";
+  copyInfo.style.opacity = "0.75";
+  copiedInfo.style.transform = "translateY(200%)";
+  copiedInfo.style.opacity = "0";
 
-    (function() {
-        qr = new QRious({
-            element: document.querySelector("#qrCode"),
-            size: 200,
-            value: "sujonmir",
-            /*
-              background: "#123456",
-              backgroundAlpha: 0.7,
-              foreground: "darkcyan",
-              foregroundAlpha: 0.5, 
-              padding: 20, 
-              level: "H",
-            */
-        });
-    })();
+  if (customText.value.length != 0) {
+    myPass = customText.value;
+    customText.value= ''
+  }
+  resultEl.innerText = myPass;
+  (function () {
+    qr = new QRious({
+      element: document.querySelector("#qrCode"),
+      size: 200,
+      value: "sujonmir",
+    });
+  })();
 
-    const generateQRCode = () => {
-        const qrText = myPass;
-        qr.set({
-            value: qrText,
-        });
-    };
-    generateQRCode()
+  const generateQRCode = () => {
+    const qrText = myPass;
+    qr.set({
+      value: qrText,
+    });
+  };
+  generateQRCode();
 });
 
 // Function responsible to generate password and then returning it.
 function generatePassword(length, lower, upper, number, symbol) {
-    let generatedPassword = "";
-    const typesCount = lower + upper + number + symbol;
-    const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(item => Object.values(item)[0]);
-    if (typesCount === 0) {
-        return "";
-    }
-    for (let i = 0; i < length; i++) {
-        typesArr.forEach(type => {
-            const funcName = Object.keys(type)[0];
-            generatedPassword += randomFunc[funcName]();
-        });
-    }
-    return generatedPassword.slice(0, length)
-        .split('').sort(() => Math.random() - 0.5)
-        .join('');
+  let generatedPassword = "";
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+  if (typesCount === 0) {
+    return "";
+  }
+  for (let i = 0; i < length; i++) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+  return generatedPassword
+    .slice(0, length)
+    .split("")
+    .sort(() => Math.random() - 0.5)
+    .join("");
 }
 
 // function that handles the checkboxes state, so at least one needs to be selected. The last checkbox will be disabled.
 function disableOnlyCheckbox() {
-    let totalChecked = [uppercaseEl, lowercaseEl, numberEl, symbolEl].filter(el => el.checked)
-    totalChecked.forEach(el => {
-        if (totalChecked.length == 1) {
-            el.disabled = true;
-        } else {
-            el.disabled = false;
-        }
-    })
+  let totalChecked = [uppercaseEl, lowercaseEl, numberEl, symbolEl].filter(
+    (el) => el.checked
+  );
+  totalChecked.forEach((el) => {
+    if (totalChecked.length == 1) {
+      el.disabled = true;
+    } else {
+      el.disabled = false;
+    }
+  });
 }
 
-[uppercaseEl, lowercaseEl, numberEl, symbolEl].forEach(el => {
-    el.addEventListener('click', () => {
-        disableOnlyCheckbox()
-    })
-})
-
+[uppercaseEl, lowercaseEl, numberEl, symbolEl].forEach((el) => {
+  el.addEventListener("click", () => {
+    disableOnlyCheckbox();
+  });
+});
 
 // qr start
-window.addEventListener('load', function() {
-    (function() {
-        qr = new QRious({
-            element: document.querySelector("#qrCode"),
-            size: 200,
-            value: "sujonmir"
-        });
-    })();
-    const generateQRCode = () => {
-        const qrText = myPass;
-        qr.set({
-            value: qrText,
-        });
-    };
-    generateQRCode()
-})
-
+window.addEventListener("load", function () {
+  (function () {
+    qr = new QRious({
+      element: document.querySelector("#qrCode"),
+      size: 200,
+      value: "sujonmir",
+    });
+  })();
+  const generateQRCode = () => {
+    const qrText = myPass;
+    qr.set({
+      value: qrText,
+    });
+  };
+  generateQRCode();
+});
 
 // generateBtn.addEventListener('click', function() {
 
